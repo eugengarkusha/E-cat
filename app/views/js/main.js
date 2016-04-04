7040edd0fe48c70ea292dbce4784fba63e0aa8d3
eugenelavr <e.lavrinenkov@gmail.com>
2016-04-04 20:13:52 +0300

    Added view files

---
+++ app/views/js/main.js
@@ -0,0 +1,91 @@
+$(function () {
+    $('.main-menu-handle').click(function(event) {
+        $(this).toggleClass('active');
+        $('.header-main-menu').toggleClass('slide');
+    });
+
+    $('.header-main').scroolly([
+        {
+            from: 'doc-top + 122px',
+            addClass: 'sticky'
+        },
+        {
+            to: 'doc-top + 1px',
+            removeClass: 'sticky'
+        }
+    ]);
+
+    if ($('.booking-form')[0] && !$('.form-reply')[0]) {
+        $('select').selectric({
+            responsive: true
+        });
+
+        Date.parseDate = function( input, format ){
+            return moment(input,format).toDate();
+        };
+        Date.prototype.dateFormat = function( format ){
+            return moment(this).format(format);
+        };
+
+        $.datetimepicker.setLocale('ru');
+        $('#checkIn, #checkOut').datetimepicker({
+            format:'DD.MM.YYYY h:mm a',
+            formatTime:'h:mm a',
+            formatDate:'DD.MM.YYYY',
+            validateOnBlur: true
+        });
+    }
+
+    $('.scroll-up').click(function (event) {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 1e3);
+    });
+
+    if ($('#map')[0]) {
+        var mapCanvas = $('#map')[0];
+        var mapOptions = {
+            center: new google.maps.LatLng(44.5403, -78.5463),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+
+    if($('#imageGallery')[0]) {
+        $('#imageGallery').lightSlider({
+            gallery: true,
+            item: 1,
+            loop: true,
+            slideMargin: 0,
+            thumbItem: 7,
+            auto: true,
+            speed: 1200,
+            pause: 5000,
+            keyPress: true,
+            pauseOnHover: true,
+            onSliderLoad: function(el) {
+                el.lightGallery({
+                    selector: '#imageGallery .lslide'
+                });
+            },
+            currentPagerPosition:'left'
+        });
+    }
+
+    $('#google-map').lightGallery({
+        selector: 'this',
+        iframeMaxWidth: '80%'
+    });
+
+    if ($('#lightgallery')[0]) {
+        $('.gallery li').each(function () {
+            $(this).hoverdir();
+        });
+
+        $('#lightgallery').lightGallery({
+            selector: 'a'
+        });
+    }
+
+});
