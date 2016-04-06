003c9944ba92510c7408a190c69181a19230ff87
eugenelavr <e.lavrinenkov@gmail.com>
2016-04-06 15:52:30 +0300

    rebase master and merge scala_templates branch

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -58,9 +58,6 @@ class Application extends Controller {
   def ekaterina2 = Action{
     Ok(views.html.pages.hotels.ekaterina2.ekaterina2())
   }
-  def reservation = Action{
-    Ok(views.html.pages.reservation())
-  }
   def gallery = Action{
     Ok(views.html.pages.gallery())
   }
