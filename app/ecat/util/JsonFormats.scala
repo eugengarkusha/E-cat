87f58dbc0608cb47f849ba2b5c3ebcc6d1dfc17d
Ievgen Garkusha <ievgen@riskident.com>
2016-05-08 12:50:47 +0200

    Possible fix for LocalTimeFormatting

--- app/ecat/util/JsonFormats.scala
+++ app/ecat/util/JsonFormats.scala
@@ -9,8 +9,17 @@ import play.api.libs.json.Writes.TemporalFormatter._
 object JsonFormats {
 
   implicit val localTimeFormat = new Format[LocalTime]{
-    override def reads(j: JsValue): JsResult[LocalTime] = j.validate[JsNumber].map(bd=>LocalTime.ofSecondOfDay(bd.value.toLong))
+    override def reads(j: JsValue): JsResult[LocalTime] = j.validate[String].map(bd=>LocalTime.parse(bd))
 
-    override def writes(o: LocalTime): JsValue = JsNumber(o.toSecondOfDay)
+    override def writes(o: LocalTime): JsValue = JsString(o.toString)
   }
+
+//unneeded
+//  val petrovichLocalTimeFormatReads =Reads[LocalTime]{js=>
+//    js.validate[String].flatMap{ hourOfDay=>
+//      try{JsSuccess(LocalTime.of(hourOfDay.toInt,  0))}
+//      catch { case t:Throwable => JsError(s"Cannot parse hourOfDay from $js: ${t.getMessage}")}
+//    }
+//  }
+
 }
