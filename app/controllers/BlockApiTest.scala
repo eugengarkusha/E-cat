4e01c1f4c4338cf79c5ef8844e51ad292e79acd7
Ievgen Garkusha <ievgen@riskident.com>
2016-09-01 21:25:01 +0200

    adding all blocking related metods and routes(todo: finish  with blockCategory route)

--- app/controllers/BlockApiTest.scala
+++ app/controllers/BlockApiTest.scala
@@ -4,30 +4,40 @@ import java.math.BigInteger
 
 import async.client.ObmenSaitPortType
 import otelsait.Zakaz
-import play.api.libs.json.JsString
-import play.api.libs.json.Json
-import play.api.libs.json.Writes
 import play.api.mvc.Action
 import play.api.mvc.Controller
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 //TODO: param names are freaky but correspond to  wsdl interface definition. Ask mr. P to rename params appropriately(and change Summa type to Double)
 class BlockApiTest(proxy: ObmenSaitPortType) extends Controller {
 
-
-  def blockCancel(id: String)= Action.async {
+  def blockCancel(id: String) = Action.async {
     Future(proxy.blockCancel(id)).map(x => Ok(x.toString))
   }
 
-  case class BlockCancelResp(tip: Boolean, id: String, timer: BigInteger)
-  implicit val bigIntegerWrites =  Writes[BigInteger](bi => JsString(bi.toString))
-  implicit val blockCancelRespWrites = Json.writes[BlockCancelResp]
-  //need Number to be a list of Zakaz
-  def blockCategory(DatN: String, DatK: String, idHotell: String,idCategory: String, idGost: String , TimeBlock: BigInteger, Number: Zakaz, Summa: Float) = Action.async{
-    Future(proxy.blockCategory(DatN, DatK, idHotell, idCategory, idGost, TimeBlock, Number, Summa))
-    .map(r=> Ok(Json.toJson(BlockCancelResp(r.isTip, r.getIdzakaza, r.getTimer))))
+  def blockContinue(id: String) = Action.async {
+    Future(proxy.blockContinue(id)).map(x => Ok(s"tip = ${x.isTip}, timer = ${x.getTimer}"))
   }
+
+  def blockPay(id: String) = Action.async {
+    Future(proxy.blockPay(id)).map(x => Ok(s"tip = ${x.isTip}, timer = ${x.getTimer}"))
+  }
+
+  def paid(id: String) = Action.async {
+    Future(proxy.paid(id)).map(b => Ok(b.toString))
+  }
+
+  def zayavka(id: String) = Action.async {
+    Future(proxy.zayvka(id)).map(b => Ok(b.toString))
+  }
+  //need Number to be a list of Zakaza(create bindable for it)
+  def blockCategory(DatN: String, DatK: String, idHotell: String, idCategory: String, idGost: String, TimeBlock: BigInteger, Number: Zakaz, Summa: Float) =
+    Action.async {
+      Future(proxy.blockCategory(DatN, DatK, idHotell, idCategory, idGost, TimeBlock, Number, Summa))
+      .map(r => Ok(s"tip = ${r.isTip}, idZakaza = ${r.getIdzakaza}, timer = ${r.getTimer}"))
+    }
+
 
 }
